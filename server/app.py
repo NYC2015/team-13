c740ca1834b8ca6d541a6ce90e1910cd4348d401
Nikhil <nkbuduma@gmail.com>
2015-10-16 19:49:46 -0400

    get product by ean endpoint added

--- server/app.py
+++ server/app.py
@@ -1,9 +1,20 @@
+from factual import Factual
+from flask import Flask, jsonify
 import os
+
+app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "We feed children~"
+    return "We feed children!"
 
+@app.route('/get_by_ean/<ean_13>', methods=['GET'])
+def get_product_by_ean(ean_13):
+    factual = Factual('pFMeueZJQpyZnSPILemUPxzNJmathJyrxoqOnow0', 'ROHZOgzy9GwJGb9egKpzAVTYZq35iuj6f3Uu4rNu')
+    nutrition = factual.table('products-cpg-nutrition').search(ean_13)
+    print nutrition.data()
+    resp = nutrition.data()[0]
+    return jsonify(**resp)
 
 
 if __name__ == '__main__':
