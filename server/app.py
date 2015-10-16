ae2cf67a11269e15e03d2dcb5d54b85a86e7e450
Nikhil <nkbuduma@gmail.com>
2015-10-16 19:32:59 -0400

    added server and virtual_env components

---
+++ server/app.py
@@ -0,0 +1,13 @@
+import os
+
+@app.route('/')
+def index():
+    return "We feed children~"
+
+
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    if port == 5000:
+        app.debug = True
+    app.run(host='0.0.0.0', port=port)
