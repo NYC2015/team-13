acaa4d424ebc85d360b57ae3457cc663551f6e0d
Nikhil <nkbuduma@gmail.com>
2015-10-16 20:17:27 -0400

    updated push product api

--- README.md
+++ README.md
@@ -17,3 +17,39 @@ Chern Yee Chua
 Linda Jing 
 
 Nikhil Buduma
+
+# API
+
+## Nutrition Facts Schema
+
+```
+{
+	u'ean13': u'0024100101481', 
+	u'sugars': 1, 
+	u'trans_fat': 0, 
+	u'protein': 3, 
+	u'size': [u'13 oz'], 
+	u'category': u'Snacks', 
+	u'ingredients': [u'Cheese Crackers (Enriched Flour (Wheat Flour', u'Niacin', u'Reduced Iron', u'Thiamin Mononitrate (Vitamin B1)', u'Riboflavin (Vitamin B2)', u'Folic Acid', u'Vegetable Oil (Canola', u'Cottonseed', u'Palm', u'Sunflower'], 
+	u'vitamin_c': 0, 
+	u'vitamin_a': 0, 
+	u'dietary_fiber': 1, 
+	u'product_name': u'Snack Mix Baked Snack Assortment', 
+	u'brand': u'Cheez It', u'cholesterol': 0, 
+	u'image_urls': [u'http://Img1.targetimg1.com/wcsstore/TargetSAS//img/p/13/00/13009791_201307191112.jpg', u'http://c3.soap.com/images/products/p/p/zqb/zqb-5635_1.jpg', u'http://ecx.images-amazon.com/images/I/51khH2ynOxL._SL500_AA300_PIbundle-4,TopRight,0,0_AA300_SH20_.jpg', u'http://media.shopwell.com/product/2410092464_full.jpg', u'http://www.scanavert.com/api/picture.php?upc=00024100101481&width=150&height=150'], 
+	u'factual_id': u'a0799ad5-aa75-40d3-bd1e-56a76ea3b1d7', 
+	u'avg_price': 7.39, 
+	u'potassium': 0, 
+	u'fat_calories': 40, 
+	u'sodium': 290, 
+	u'calories': 120, 
+	u'upc': u'024100101481', 
+	u'sat_fat': 1, 
+	u'calcium': 2, 
+	u'total_fat': 4.5, 
+	u'serving_size': u'0.5 cup', 
+	u'iron': 6, 
+	u'servings': 12, 
+	u'total_carb': 21
+}
+```
--- server/app.py
+++ server/app.py
@@ -1,6 +1,8 @@
 from factual import Factual
 from flask import Flask, jsonify
 import os
+from firebase import firebase
+
 
 app = Flask(__name__)
 
@@ -12,9 +14,21 @@ def index():
 def get_product_by_ean(ean_13):
     factual = Factual('pFMeueZJQpyZnSPILemUPxzNJmathJyrxoqOnow0', 'ROHZOgzy9GwJGb9egKpzAVTYZq35iuj6f3Uu4rNu')
     nutrition = factual.table('products-cpg-nutrition').search(ean_13)
-    print nutrition.data()
     resp = nutrition.data()[0]
     return jsonify(**resp)
+
+@app.route('/push_product/<org_id>/<ean_13>/<int:quantity>', methods=['GET'])
+def push_product(org_id, ean_13, quantity):
+    fb = firebase.FirebaseApplication('https://feedthechildren.firebaseio.com/', None)
+    exists = fb.get("/inventory/" + org_id, None, params={'print': 'pretty'}, headers={'X_FANCY_HEADER': 'VERY FANCY'})
+    if exists is not None and ean_13 in exists:
+        result = fb.put('/inventory/' + org_id + '/' + ean_13, 'quantity', str(int(exists[ean_13]['quantity']) + quantity), params={'print': 'pretty'}, headers={'X_FANCY_HEADER': 'VERY FANCY'})
+        return str(result)
+    factual = Factual('pFMeueZJQpyZnSPILemUPxzNJmathJyrxoqOnow0', 'ROHZOgzy9GwJGb9egKpzAVTYZq35iuj6f3Uu4rNu')
+    nutrition = factual.table('products-cpg-nutrition').search(ean_13)
+    result = fb.put('/inventory/' + org_id, ean_13, {'quantity': str(quantity), 'metadata' : nutrition.data()[0]}, params={'print': 'pretty'}, headers={'X_FANCY_HEADER': 'VERY FANCY'})
+    return str(result)
+
 
 
 if __name__ == '__main__':
