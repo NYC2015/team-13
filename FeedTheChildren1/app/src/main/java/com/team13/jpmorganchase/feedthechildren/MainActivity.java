c702c4ce9026da5f8fad5fd705988c196d6f4410
Jarret Hutchison <jarrethutchison@Jarrets-MacBook-Pro.local>
2015-10-17 04:02:27 -0400

    added in favorites tab

---
+++ FeedTheChildren1/app/src/main/java/com/team13/jpmorganchase/feedthechildren/MainActivity.java
@@ -0,0 +1,75 @@
+package com.team13.jpmorganchase.feedthechildren;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import com.parse.ParseUser;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ParseUser currentUser = ParseUser.getCurrentUser();
+
+        if(currentUser != null){            //Already logged in (current user exists)
+            goToLoggedInPage();
+        } else {                            //Need to log in
+            goToLoginPage();
+        }
+
+    }
+
+
+    @Override
+    protected void onPause(){           //Kill this activity after login
+        super.onPause();
+        this.finish();
+    }
+
+    public void goToLoginPage(){
+        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+        startActivity(intent);
+    }
+
+    public void goToLoggedInPage(){
+        if(Utility.checkNewEntry()){
+            Utility.setChangedRecord();
+            Utility.generateFriendList(ParseUser.getCurrentUser());
+        }
+        Intent intent = new Intent(MainActivity.this, ContentActivity.class);
+        startActivity(intent);
+    }
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
