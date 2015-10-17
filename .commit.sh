b6709564d3a05c9d22266e102684ffbed2f8d3d9
Nikhil <nkbuduma@gmail.com>
2015-10-17 02:26:52 -0400

    updating script

--- .commit.sh
+++ .commit.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-git add .
-git commit -m "updating script"
-git pull --rebase
-git push origin master
+
+for i in `seq 1 10`;
+do
+    echo "hi" >> .hidden
+    git add .
+    git commit -m "updating script"
+    git pull --rebase
+    git push origin master
+done
